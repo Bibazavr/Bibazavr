7323124e8585e6db65e3c6f701095d5c454d2be4
Bibazavr <lirik.hariev@gmail.com>
2024-05-24 22:03:18 +0300

    chore: add screens

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
 
+import { Screens } from "@/shared/constants/Screens";
+
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name={Screens.Home} />
     </Stack>
   );
 }
